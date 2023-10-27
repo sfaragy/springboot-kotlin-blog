318806de2ee7fd845d015dcbd235df70dfdd3f7f
Soliman Faragy <soliman.faragy@lifullconnect.com>
2023-10-27 14:17:22 +0700

    Add Entity

---
+++ src/main/kotlin/first/blog/firstblog/entities/User.kt
@@ -0,0 +1,13 @@
+package first.blog.firstblog.entities
+
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "users")
+data class User(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long,
+    val name: String,
+    val email: String
+)
