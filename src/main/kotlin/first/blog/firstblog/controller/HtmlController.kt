22bd92562dd304ef90e6145be355f991d1116174
Soliman Faragy <soliman.faragy@lifullconnect.com>
2023-10-27 14:16:28 +0700

    Add new route with post ID

--- src/main/kotlin/first/blog/firstblog/controller/HtmlController.kt
+++ src/main/kotlin/first/blog/firstblog/controller/HtmlController.kt
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 
 @Controller
 class HtmlController {
@@ -11,6 +12,12 @@ class HtmlController {
     @GetMapping("/")
     fun blog(model: Model): String {
         model["title"] = "Blog"
+        return "blog"
+    }
+
+    @GetMapping("/blog/details/{id}")
+    fun edit(model: Model, @PathVariable("id") id:String): String {
+        model["title"] = "Blog content - $id"
         return "blog"
     }
 
